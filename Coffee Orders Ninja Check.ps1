019d4bddc812040bbcf06d8f2356a96f0c249477
Alan Sonnheim <alan.sonnheim@thermtech.net>
2026-06-18 10:26:45 -0500

    Add .ps1 file

---
+++ Coffee Orders Ninja Check.ps1
@@ -0,0 +1,89 @@
+$EnvFilePath = ".\.env.local"
+
+If (-not (Test-Path $EnvFilePath -PathType Leaf)) {
+    Write-Error "Error: .env.local file not found at $($EnvFilePath)"
+    Exit 1
+}
+
+$Env = @{}
+
+$EnvLines = Get-Content $EnvFilePath
+
+ForEach ($Line in $EnvLines) {
+    $TrimmedLine = $Line.Trim()
+
+    If ([string]::IsNullOrEmpty($TrimmedLine) -or $TrimmedLine.StartsWith('#')) {
+        Continue
+    }
+
+    $Name, $Value = $TrimmedLine -split '=', 2
+    $Env.Add($Name, $Value)
+}
+
+$Body = @{
+    grant_type    = "client_credentials"
+    client_id     = $Env["CLIENT_ID"]
+    client_secret = $Env["CLIENT_SECRET"]
+    scope         = "monitoring"
+}
+
+$TokenResponse = Invoke-RestMethod -Method POST -Uri ("$($Env["RMM_URL"])/ws/oauth/token") -ContentType "application/x-www-form-urlencoded" -Body $Body
+
+$Token = $TokenResponse.access_token
+
+$Headers = @{
+    Authorization = "Bearer $Token"
+    Accept        = "application/json"
+}
+
+$Devices = Invoke-RestMethod -Method GET -Uri "$($Env["RMM_URL"])/v2/devices" -Headers $Headers
+
+$DevicesJSON = $Devices | ConvertTo-Json
+
+$DevicesArray = ConvertFrom-Json $DevicesJSON
+
+$NinjaComputerList = @()
+
+For ($Index = 0; $Index -lt $DevicesArray.Count; $Index++) {
+    $DeviceName = ($DevicesArray[$Index].dnsName -replace $($Env["DNS_NAME"]), "")
+
+    # Check for no name to not add devices that were recently deleted
+    If ($DeviceName -ne "") {
+        $NinjaComputerList += ($DevicesArray[$Index].dnsName -replace $($Env["DNS_NAME"]), "")
+    }
+}
+
+$ReportPath = $Env["REPORT_URL"]
+
+$ExcelObj = Import-Excel -Path $ReportPath
+
+$ExcelPackage = Open-ExcelPackage -Path $ReportPath
+
+$SheetNames = $ExcelPackage.Workbook.Worksheets.Name
+
+$CoffeeSheetsComputerList = @()
+
+$TrackedSheets = @($Env["TRACKED_SHEETS"].Split(','))
+
+ForEach ($Sheet in $SheetNames) {
+    If ($TrackedSheets -contains $Sheet) {
+        $Names = Import-Excel -Path $ReportPath -WorksheetName $Sheet | Select-Object -ExpandProperty Name
+        $CoffeeSheetsComputerList += $Names 
+    }   
+}
+
+ForEach ($CoffeeComputer in $CoffeeSheetsComputerList) {
+    If ($NinjaComputerList -contains $CoffeeComputer) {
+
+    } Else {
+        Write-Host $CoffeeComputer "is present in Coffee Orders but not in Ninja!" -BackgroundColor DarkGray
+    }
+}
+
+ForEach ($NinjaComputer in $NinjaComputerList) {
+    If ($CoffeeSheetsComputerList -contains $NinjaComputer) {
+
+    } Else {
+        Write-Host $NinjaComputer "is present in Ninja but not in Coffee Orders!" -BackgroundColor Blue
+    }
+}
